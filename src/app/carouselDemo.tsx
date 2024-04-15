cfbd225ecdd57a6c6f37964386f2b1cfb640eeab
ju717 <julia.ha2g@outlook.com>
2024-04-15 11:58:54 +0200

    2
    efa

--- src/app/carouselDemo.tsx
+++ src/app/carouselDemo.tsx
@@ -23,6 +23,17 @@ export function CarouselDemo() {
           <script async type="module" src="https://embed.styledcalendar.com/assets/parent-window.js"></script>
           </div>
           </CarouselItem>
+          <CarouselItem >
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2716.3209826460047!2d15.400210776775966!3d47.09277782331738!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476e344b367fde5d%3A0xfc357c118d3051f7!2sH%C3%B6here%20Technische%20Bundeslehranstalt%20(HTL)%20Graz-G%C3%B6sting!5e0!3m2!1sde!2sat!4v1706190074713!5m2!1sde!2sat"
+            width="800"
+            height="200"
+            loading="lazy"
+          ></iframe>
+        <iframe src="https://www.wetter.de/widget/3tage/u26gz2cz/false/">
+        </iframe>
+
+          </CarouselItem>
    
       </CarouselContent>
       <CarouselPrevious />
