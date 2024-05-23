16b19352250109c8dd2eb6d77b305823fef842fa
julia.haag <julia.haag>
2024-05-23 15:26:36 +0200

    alskdjf

---
+++ gpioserver.py
@@ -0,0 +1,18 @@
+from flask import Flask, request
+#import RPi.GPIO as GPIO
+
+app = Flask(__name__)
+
+#GPIO.setmode (GPIO.BCM)
+#GPIO.setup (17, GPIO.OUT)
+
+def turn_on_pin():
+    print(f"Pin turned on with akkustand")
+    #GPIO.output(17, GPIO.HIGH)
+
+
+@app.route('/optimisiertesLaden', methods=['POST'])
+def handle_akkustand():
+    turn_on_pin()
+    return "Pin status updated (simulated)"
+app.run(debug=True)
