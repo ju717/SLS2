17bb5b0e5c128ce8a78f4ab85fd501518cf10f1c
julia.haag <julia.haag>
2024-06-14 10:58:33 +0200

    aef

--- gpioserver.py
+++ gpioserver.py
@@ -1,19 +1,18 @@
 from flask import Flask, request
-import RPi.GPIO as GPIO
+#import RPi.GPIO as GPIO
 from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
-GPIO.setmode (GPIO.BCM)
-GPIO.setup (37, GPIO.OUT)
+#GPIO.setmode (GPIO.BCM)
+#GPIO.setup (37, GPIO.OUT)
 
 def turn_on_pin():
     print(f"Pin turned on with akkustand")
-    GPIO.output(37, GPIO.HIGH)
+    #GPIO.output(37, GPIO.HIGH)
 
-#jfasle
-@app.route('/optimisiertesLaden')
+@app.route('/optimisiertesLaden', methods=['POST'])
 @cross_origin()
 def handle_akkustand():
     turn_on_pin() 
